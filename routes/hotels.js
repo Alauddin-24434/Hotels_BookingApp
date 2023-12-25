163fb44213771b28885bc3146789bf56355c75f4
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2023-12-25 21:35:39 +0600

    secessfully hotel create ,update, delete , get , get all by insomnia

--- routes/hotels.js
+++ routes/hotels.js
@@ -25,7 +25,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
  
     try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body})
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body},{new:true})
         res.status(200).json(updatedHotel)
     }
     catch (err) {
@@ -39,8 +39,8 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
  
     try {
-        const deletedHotel = await Hotel.findByIdAndDelete(req.params.id, {$set:req.body})
-        res.status(200).json(deletedHotel)
+        await Hotel.findByIdAndDelete(req.params.id)
+        res.status(200).json("Hotel has been deleted")
     }
     catch (err) {
         res.status(500).json(err)
@@ -48,7 +48,32 @@ router.delete('/:id', async (req, res) => {
 })
 
 // GET
+
+
+router.get('/:id', async (req, res) => {
+ 
+    try {
+       const hotel= await Hotel.findById(req.params.id)
+        res.status(200).json(hotel)
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 // GET ALL
+
+router.get('/', async (req, res) => {
+ 
+    try {
+       const hotels= await Hotel.find()
+        res.status(200).json(hotels)
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 
 
 
