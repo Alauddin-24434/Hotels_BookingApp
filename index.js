1d4abaff24a06877a1802bfad754d8c8dcc98b06
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2023-12-25 15:41:59 +0600

    added routes for auth, hotels, rooms, users.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import authRoute from './routes/auth.js'
+import usersRoute from './routes/users.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
 const app = express();
 dotenv.config()
 
@@ -16,6 +20,13 @@ const connect= async ()=>{
 mongoose.connection.on("disconnected", ()=>{
     console.log("MongoDb disconnected")
 })
+
+// middleware
+app.use('api/auth', authRoute)
+app.use('api/auth', usersRoute)
+app.use('api/auth', hotelsRoute)
+app.use('api/auth', roomsRoute)
+
 
 app.listen(8000, () => {
     connect()
