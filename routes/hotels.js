3d1fe05e8a73390bfe4ea9d7249c632ae2aa6656
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2023-12-25 21:01:51 +0600

    successfuly delte single house findbyId and delete

--- routes/hotels.js
+++ routes/hotels.js
@@ -34,6 +34,19 @@ router.put('/:id', async (req, res) => {
 })
 
 // DELETE
+
+
+router.delete('/:id', async (req, res) => {
+ 
+    try {
+        const deletedHotel = await Hotel.findByIdAndDelete(req.params.id, {$set:req.body})
+        res.status(200).json(deletedHotel)
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 // GET
 // GET ALL
 
